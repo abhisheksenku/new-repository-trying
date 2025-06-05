f2177c14bd96132a87c1566b6d13e7954c48a312
abhisheksenku <abhi971255@gmail.com>
2025-06-05 09:01:13 +0530

    Initial commit of expense tracker task files

---
+++ signup.js
@@ -0,0 +1,19 @@
+document.getElementById('signupForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // const password = document.getElementById('password').value;
+    // const confirmPassword = document.getElementById('confirmPassword').value;
+    
+    // if (password !== confirmPassword) {
+    // alert('Passwords do not match!');
+    // return;
+    // }
+
+    console.log('Form submitted:', {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    password: password
+    });
+
+    alert('Signup successful!');
+    this.reset();
+});
