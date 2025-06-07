5560a638a7dad23d61a11335e75f9b0bbd341724
Abhishek Uthukotasriram <62733295+abhisheksenku@users.noreply.github.com>
2025-06-07 14:31:34 +0530

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,19 +1,19 @@
 document.getElementById('signupForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    // const password = document.getElementById('password').value;
-    // const confirmPassword = document.getElementById('confirmPassword').value;
+//   const password = document.getElementById('password').value;
+//   const confirmPassword = document.getElementById('confirmPassword').value;
     
-    // if (password !== confirmPassword) {
-    // alert('Passwords do not match!');
-    // return;
-    // }
-
+//   if (password !== confirmPassword) {
+//     alert('Passwords do not match!');
+//     return;
+//   }
+    
     console.log('Form submitted:', {
     name: document.getElementById('name').value,
     email: document.getElementById('email').value,
     password: password
     });
-
-    alert('Signup successful!');
+    
+    alert('Signup successful! (This is a frontend demo)');
     this.reset();
 });
